99516b7b3e62cd487caf58d4527db83a711480b8
Anup Suresh <anupsuresh216@gmail.com>
2025-08-13 16:56:03 +0530

    added backend for getting presigned urls for image upload from frontend and also did a bunch of setup and auth and course creation etc

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,20 +1,73 @@
-const express = require("express");
+const { z } = require("zod");
+const CourseModel = require("../models/Course");
 
-const preview = async (req, res) => {
+const purchaseCourse = async (req, res) => {
    try {
    } catch (error) {}
 };
-const purchase = async (req, res) => {
+const ownedCourses = async (req, res) => {
    try {
    } catch (error) {}
 };
-const purchases = async (req, res) => {
+
+const previewCourses = async (req, res) => {
+   try {
+   } catch (error) {}
+};
+const createCourse = async (req, res) => {
+   try {
+      const courseBody = z.object({
+         title: z.string().min(2).max(30),
+         description: z.string().min(2).max(500),
+         price: z.number().min(1).max(99999),
+         // imageUrl: z.string(),
+      });
+
+      const validationResult = await courseBody.safeParseAsync(req.body);
+
+      if (!validationResult.success) {
+         return res
+            .status(422)
+            .json({ error: z.treeifyError(validationResult.error) });
+      }
+
+      const { title, description, price } = validationResult.data;
+      const userId = req.user._id;
+
+      
+
+      const course = new CourseModel({
+         title,
+         description,
+         price,
+         creatorId: userId,
+      });
+      await course.save();
+
+      res.status(200).json({
+         message: "Course created successfully.",
+      });
+   } catch (error) {
+      return res.status(500).json({
+         error: "Internal server error while creating course.",
+         details: error.message,
+      });
+   }
+};
+const updateCourse = async (req, res) => {
+   try {
+   } catch (error) {}
+};
+const deleteCourse = async (req, res) => {
    try {
    } catch (error) {}
 };
 
 module.exports = {
-   preview,
-   purchase,
-   purchases,
+   createCourse,
+   updateCourse,
+   deleteCourse,
+   previewCourses,
+   purchaseCourse,
+   ownedCourses,
 };
